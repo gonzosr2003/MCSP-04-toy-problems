0caf5f22914fde220bfb96cc580fb93fa40c6212
Tye <tyemacon@gmail.com>
2020-02-14 13:21:01 -0600

    commonChars added

--- evenOccurence/evenOccurence.js
+++ evenOccurence/evenOccurence.js
@@ -11,30 +11,5 @@
  */
 
 var evenOccurrence = function(arr) {
-  //create a dictionary object
-  let result = null;
-  let dict = {};
-  //loop through array
-  for (let i = 0; i < arr.length; i++) {
-    //determine if that item in the array is already in the object as a key
-    if (dict[arr[i]]) {
-      //if yes, increment the value
-      result = arr[i];
-      dict[arr[i]] = 2;
-      //if not, add it as a key to the object with a value of 1
-    } else {
-      dict[arr[i]] = 1;
-    }
-  }
-  //loop through object and return the first instance of value greater than 1
-  for (let key in dict) {
-    if (dict[key] === 2) {
-      if (Number(key)) {
-        return Number(key);
-      } else {
-        return key;
-      }
-    }
-  }
-  return result;
+  // Your work here!
 };
