eed822cb25ce0e04a112cee52bba8fdee8e0674a
Mike Gonzales <michael.gonzales5705@gmail.com>
2020-03-19 09:57:20 -0500

    allAnagrams updated

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -14,13 +14,25 @@
 
 var allAnagrams = function(string) {
   // Your code here.
-  // make a recurison method
-  function NeedMoreOfficeHoursOnRecursion () {
-    // iterate over string
-    for (var i = 0; i < string.length; i++) {
-      // inital letter + next
-      // 
+  // use a recurison method
+  function NeedMoreOfficeHoursOnRecursion (start, leftover) {
+    //base case
+    if (leftover.length === 1) {
+      return [start + leftover];
+    } else {
+        // output them as an aray
+        var output =[];
+      // iterate over string
+      for (var i = 0; i < leftover.length; i++) {
+        // call recursion on self 
+        //use i as start; will use first i / need i plus next letter    
+        var startPlusNext = NeedMoreOfficeHoursOnRecursion(leftover[i], leftover.substr(0, i) + leftover.substr(i + 1))
+        // push to array
+        output.push(start + leftover);
+      }
+       // return array
+  return output;
     }
-  }
+  } 
 };
 // The substr() method extracts parts of a string, beginning at the character at the specified position, and returns the specified number of characters.
