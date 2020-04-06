6538accd71ae390295e6d0cb57b905c8ba91c8c8
Mike Gonzales <michael.gonzales5705@gmail.com>
2020-04-06 09:58:04 -0500

    gonzo step by step for binary search

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -11,5 +11,34 @@
  */
 
 var binarySearch = function(array, target) {
+    // create var for array
+        //low index // 0
+    let low = 0;
+        //high index //4
+    let high = array.length - 1;
+    // if low is less than high
+    if(low < high)
+        //where is the middle index?
+            // create var for middle
+                // array length / 2
+    //if element at middle var is target
+    // yes - return middle
+    // no - find out which side of array to continue search
+    // if key is less than mid
+    // make high var equal to mid -1
+    // if key is greater than mid
+    // make low var equal to mid + 1
+    // resets search area in half
+    
+    // can i use recursion here? how do i implement that. 
+    
+    // create var for target
+        // key is = to target
+    // if target key is not in array
+    // return null
 
 };
+// I- sorted array
+// O- target index
+// C- none
+// E- if target not found return null
