231389a2b5e9336d9d4f29074916d575b679ed27
Mike Gonzales <michael.gonzales5705@gmail.com>
2020-04-30 09:57:06 -0500

    psuedo

--- balancedBrackets/balancedBracets.js
+++ balancedBrackets/balancedBracets.js
@@ -9,4 +9,28 @@
 // str: "[{()}]"                  |   true
 // str: "(50)("                   |   false
 // str: "[{]}"                    |   false
-const isBalanced = str => {};
+const isBalanced = str => {
+// edge case if empty string
+        // return true 
+    
+// breakdown the string
+    // create new variable 
+    // use split method
+// create an obj var
+    // will have count and type 
+// create a obj Array Var  
+// create obj count var
+// iterate over the new variable 
+// push into Array Var by type and start at 0 
+    // if type exists +1 to count
+// compare
+    // if type count is odd 
+        // return false
+    // if type count is even
+        // return true
+};
+
+// I - string
+// O - true
+// C - 
+// E - empty string,
