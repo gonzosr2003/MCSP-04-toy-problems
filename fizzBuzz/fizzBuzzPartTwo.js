b1e97c8f21f44eaf714748a8f66ff88661109221
Mike Gonzales <michael.gonzales5705@gmail.com>
2020-06-02 09:49:25 -0500

    gonzo

---
+++ fizzBuzz/fizzBuzzPartTwo.js
@@ -0,0 +1,27 @@
+// Write a function that returns a (custom) FizzBuzz sequence of the numbers 1 to 100.
+
+// The function should be able to take up to 4 arguments:
+
+// The 1st and 2nd arguments are strings, "Fizz" and "Buzz" by default;
+// The 3rd and 4th argyments are integers, 3 and 5 by default.
+
+const fizzBuzzTwo = (str1, str2, intOne, intTwo) => {
+    // create a catch array
+    let sequence = [];
+    // iterate thru array
+    for (i = 0; i < 100; i++) {
+       
+        // if i / by 3 insert "Fizz"
+        if (i % 3 === 0) {
+
+        }
+        // if i / by 5 insert "Buzz"
+
+    }
+
+}
+
+// I - 2 strings and 2 int 
+// O - sequence of #'s
+// C - 
+// E -
