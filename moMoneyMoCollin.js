8b3847273e48567cbb1b9f0bfa0a6ccf576de6e9
Mike Gonzales <michael.gonzales5705@gmail.com>
2020-06-04 09:46:45 -0500

    moMoneyMoCollins

---
+++ moMoneyMoCollin.js
@@ -0,0 +1,28 @@
+// Create a function that takes in an integer array and return the highest product of three numbers.
+// Example Inputs:
+
+// [10 10 10 10] 
+// [-5 9 6 0]
+// [2 -2 4 0 -1 3]
+// Output:
+// [30]
+// [1000]
+// [0]
+// [24]
+
+const moMoneyMoCollin = (arr) => {
+ // create a sum variable
+ let sum = 0;
+ // math max var
+ let max = Math.max(...arr);
+// find and store the 3 highest numbers
+// multiple highest by 2 highest
+// multiple that sum by lowest
+// return sum
+return sum;
+}
+
+// I - array of integers
+// O - highest sum of 3 numbers
+// C - 
+// E - 
